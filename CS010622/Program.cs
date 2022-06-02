49c1d9e374fca6f611ba44ae684b3978c05b75d8
Silvio L. Ortega <syuvyu76@gmail.com>
2022-06-01 22:35:12 -0300

    criação de dados

--- CS010622/Program.cs
+++ CS010622/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace CS010622
 {
@@ -10,7 +12,28 @@ namespace CS010622
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Olá mundo");
+            MySqlConnection conexao = new MySqlConnection("server=localhost;database=phpmysql;uid=root;password=;port=3308");
+            try
+            {
+                conexao.Open();
+            }
+            catch(MySqlException e)
+            {
+                Console.WriteLine("e.Message");
+                Console.WriteLine("Ocorreu um Erro");
+                Environment.Exit(0);
+            }
+            string sql = "select * from login";
+            MySqlCommand cmd = new MySqlCommand(sql, conexao);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            if (rdr.HasRows)
+            {
+                Console.WriteLine("Lista de Login");
+            }else
+            {
+                Console.WriteLine("Sem Dados para exibição");
+            }
+            Console.ReadLine();
         }
     }
 }
